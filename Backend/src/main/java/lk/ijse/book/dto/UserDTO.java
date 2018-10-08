e61ab38a22fd9d7e9d4a81982681238c999e68c3
Imalka1 <31820122+Imalka1@users.noreply.github.com>
2018-10-09 00:52:48 +0530

    gg

--- Backend/src/main/java/lk/ijse/book/dto/UserDTO.java
+++ Backend/src/main/java/lk/ijse/book/dto/UserDTO.java
@@ -1,4 +1,52 @@
 package lk.ijse.book.dto;
 
 public class UserDTO {
+    private String userID;
+    private String userName;
+    private String userPassword;
+    private String accountType;
+    private boolean authenticate;
+
+    public UserDTO() {
+    }
+
+    public String getUserID() {
+        return userID;
+    }
+
+    public void setUserID(String userID) {
+        this.userID = userID;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getUserPassword() {
+        return userPassword;
+    }
+
+    public void setUserPassword(String userPassword) {
+        this.userPassword = userPassword;
+    }
+
+    public String getAccountType() {
+        return accountType;
+    }
+
+    public void setAccountType(String accountType) {
+        this.accountType = accountType;
+    }
+
+    public boolean isAuthenticate() {
+        return authenticate;
+    }
+
+    public void setAuthenticate(boolean authenticate) {
+        this.authenticate = authenticate;
+    }
 }
---
+++ Frontend/src/app/dtos/user.ts
@@ -0,0 +1,7 @@
+export class User {
+  userID: string;
+  userName: string;
+  userPassword: string;
+  accountType: string;
+  authenticate: boolean;
+}
